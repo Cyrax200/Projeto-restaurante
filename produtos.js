9e55d9cb9956ccd2f8ed52f957db5d1f2e34eb4d
alan <alan-slip20@hotmail.com>
2021-12-05 13:52:52 -0300

    texto atualizado

--- produtos.js
+++ produtos.js
@@ -2,7 +2,7 @@ const produtos = [
     {
         image: src="imgs/opcao2.jpg",
         id: 1,
-        title: 'produto 01',
+        title: 'Produto 01',
         description: 'Old Burger',
         valor: " R$ 10.00"
 
@@ -10,7 +10,7 @@ const produtos = [
     {
         image: src="imgs/opcao1.jpg",
         id: 2,
-        title: 'produto 02',
+        title: 'Produto 02',
         description: 'Best Burguer Gourmet.',
         valor: " R$ 15.00"
 
@@ -18,7 +18,7 @@ const produtos = [
     {
         image: src="imgs/opcao3.jpg",
         id: 3,
-        title: 'produto 03',
+        title: 'Produto 03',
         description: 'Menca Burger',
         valor: " R$ 20.00"
 
@@ -26,7 +26,7 @@ const produtos = [
     {
         image: src="imgs/opcao04.jpg",
         id: 4,
-        title: 'produto 04',
+        title: 'Produto 04',
         description: 'Chicago Burger',
         valor: " R$ 25.00"
 
